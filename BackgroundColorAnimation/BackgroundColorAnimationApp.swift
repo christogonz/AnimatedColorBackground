8dd5b260933cd5481a1e6291750631802c1070c9
Chris Gonzalez <christogonz92@gmail.com>
2024-07-27 16:19:09 +0200

    Initial Commit

---
+++ BackgroundColorAnimation/BackgroundColorAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundColorAnimationApp.swift
+//  BackgroundColorAnimation
+//
+//  Created by Christopher Gonzalez on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundColorAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
